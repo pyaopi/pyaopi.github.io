e2bda5592feb2aaf1aa275d526cb276a85e736b6
pyaopi <43828243+pyaopi@users.noreply.github.com>
2018-10-03 17:53:11 -0400

    new

--- site.js
+++ site.js
@@ -1,21 +1,18 @@
-function EasyPeasyParallax() {
-	scrollPos = $(this).scrollTop();
-	$('.p1').css({
-		'background-position' : '50% ' + (-scrollPos/4)+"px"
-	});
-  $('.p2').css({
-		'background-position' : '50% ' + (-scrollPos/8)+"px"
-	});
-  $('.p3').css({
-		'background-position' : '70% ' + (-scrollPos/16)+"px"
-	});
-	$('.parallax-text').css({
-		'margin-top': (scrollPos/2)+"px",
-		'opacity': 1-(scrollPos/230)
-	});
-}
 $(document).ready(function(){
-	$(window).scroll(function() {
-		EasyPeasyParallax();
-	});
-});
+
+    $(document).scroll(function(){
+
+      var footerSelector = '#footer';
+      var socialBarSelector = '.icon-bar';
+
+      var bottomViewPort = $(window).scrollTop()+$(window).height();
+      var footerTop = $(footerSelector ).offset().top;
+
+      if( bottomViewPort>=footerTop){
+        $(socialBarSelector).fadeOut();
+      }else{
+        $(socialBarSelector).fadeIn();
+      }
+    });
+
+  });
